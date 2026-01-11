195dfdc7d62bd981472f74938045f8072495d858
CristianOrto <c.orto@gingerapplications.com>
2026-01-11 11:48:39 +0000

    add medusa redis cache

--- medusajs/medusa-config.ts
+++ medusajs/medusa-config.ts
@@ -66,29 +66,35 @@ module.exports = defineConfig({
             ]
           }
         },
-    {
-       resolve: "@medusajs/medusa/cache-redis",
-       options: {
-        redisUrl: REDIS_URL,
-       },
-      },
-      {
-       resolve: "@medusajs/medusa/event-bus-redis",
-       options: {
-        redisUrl: REDIS_URL,
-       }
-      },
-      {
-       resolve: "@medusajs/medusa/workflow-engine-redis",
-       options: {
-        redis: {
-         url: REDIS_URL,
-        }
-       }
-    },
-     {
-      resolve: "./src/modules/product-review"
-    },
+
+        ...(REDIS_URL ? [
+          {
+            key: Modules.CACHE,
+              resolve: '@medusajs/medusa/cache-redis',
+              options: {
+                redisUrl: REDIS_URL
+              }
+          },
+          {
+              key: Modules.EVENT_BUS,
+              resolve: '@medusajs/event-bus-redis',
+              options: {
+                redisUrl: REDIS_URL
+              }
+            },
+            {
+              key: Modules.WORKFLOW_ENGINE,
+              resolve: '@medusajs/workflow-engine-redis',
+              options: {
+                redis: {
+                  url: REDIS_URL,
+                }
+              }
+            }] : []),
+
+        {
+          resolve: "./src/modules/product-review"
+        },
 
    ],
 })
