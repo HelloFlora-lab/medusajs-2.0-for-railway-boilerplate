f21902148cd1962a167541a5a078778261ecd950
CristianOrto <c.orto@gingerapplications.com>
2026-01-15 16:21:32 +0000

    add config constants

--- helloflora_backend/medusa-config.ts
+++ helloflora_backend/medusa-config.ts
@@ -2,15 +2,40 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+
+import {
+  ADMIN_CORS,
+  AUTH_CORS,
+  BACKEND_URL,
+  COOKIE_SECRET,
+  DATABASE_URL,
+  JWT_SECRET,
+  REDIS_URL,
+  SENDGRID_API_KEY,
+  SENDGRID_FROM_EMAIL,
+  SHOULD_DISABLE_ADMIN,
+  STORE_CORS,
+  STRIPE_API_KEY,
+  STRIPE_WEBHOOK_SECRET,
+  WORKER_MODE,
+  MINIO_ENDPOINT,
+  MINIO_ACCESS_KEY,
+  MINIO_SECRET_KEY,
+  MINIO_BUCKET,
+  SLACK_WEBHOOK_URL,
+  SLACK_ADMIN_URL
+} from './src/lib/constants';
+
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: DATABASE_URL,
+    redisUrl: REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      storeCors: STORE_CORS!,
+      adminCors: ADMIN_CORS!,
+      authCors: AUTH_CORS!,
+      jwtSecret: JWT_SECRET || "supersecret",
+      cookieSecret: COOKIE_SECRET || "supersecret",
     }
   }
 })
